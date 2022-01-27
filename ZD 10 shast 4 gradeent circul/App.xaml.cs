de00616273501ad0c9df9a9a6c4c31e324e44f1c
Александр Иншаков <a.inshakov84@gmail.com>
2022-01-27 17:58:38 +0300

    Добавьте файлы проекта.

---
+++ ZD 10 shast 4 gradeent circul/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ZD_10_shast_4_gradeent_circul
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
